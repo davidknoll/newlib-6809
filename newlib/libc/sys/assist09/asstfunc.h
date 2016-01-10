4bb905b030d5c7959c99be635a2890b2b3503793
David Knoll <david@davidknoll.me.uk>
2016-01-10 12:02:01 +0000

    Better stack discipline and calling of dtors on all exit()s, not just returns from main

--- newlib/libc/sys/assist09/asstfunc.h
+++ newlib/libc/sys/assist09/asstfunc.h
@@ -2,7 +2,7 @@
  * C-callable wrappers for SBUG/ASSIST09 functions for David's 6309-6829 SBC
  */
 
-extern void _exit(int status);
+extern void __attribute__((noreturn)) _exit(int status);
 extern void seroutb(const unsigned char c);
 extern unsigned char serinb(void);
 extern unsigned char serinst(void);
